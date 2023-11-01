dc12851a74723e1b35246dd4aae453ede4bc4620
mediumexe <149566514+mediumexe@users.noreply.github.com>
2023-11-01 10:44:22 +0300

    bombim.pas

---
+++ bombim.pas
@@ -0,0 +1,12 @@
+program zad1;
+var a,b,c,d,u,X: integer;
+begin
+  write('Введите трёхзначное число:  ');
+  read(a);
+  b:= a div 100;
+  c:= (a div 10) mod 10;
+  d:= a mod 10;
+  u:= ((d*100)+(c*10)+(b));
+  X:=a*u;
+  write(X);
+end.
